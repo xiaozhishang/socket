d6bfb344963ecee2161220a6c8cd0b9e9d2562de
Lu <xiaozhishang1108@outlook.com>
2020-03-01 13:12:30 +0800

    中文

--- socket/InitSock.h
+++ socket/InitSock.h
@@ -6,7 +6,8 @@
 int main_c();
 int main_s();
 
-#pragma comment(lib, "WS2_32")  // ���ӵ�WS2_32.lib 
+// 链接到WS2_32.lib
+#pragma comment(lib, "WS2_32") 
 #ifdef _InitSock
 #endif  
 
@@ -15,17 +16,18 @@ class CInitSock
 public:
     CInitSock(BYTE minorVer = 2, BYTE majorVer = 2)
     {
-        // ��ʼ��WS2_32.dll   
+        // 初始化WS2_32.dll   
         WSADATA wsaData;
         WORD sockVersion = MAKEWORD(minorVer, majorVer);
         if (::WSAStartup(sockVersion, &wsaData) != 0) {
             exit(0);
         }
     }
-    
+
     ~CInitSock()
     {
         ::WSACleanup();
     }
 };
-CInitSock initSock;     // ��ʼ��Winsock��  
+// 初始化Winsock库  
+CInitSock initSock;
